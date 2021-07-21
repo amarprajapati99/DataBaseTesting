ded54bbf5a71c0b7c1a672f46c8114a27f10efaf
amarprajapati99 <amarprajapati99@gmail.com>
2021-07-21 21:45:11 +0530

    refactor code

--- src/test/java/com/database/testing/DBTesting.java
+++ src/test/java/com/database/testing/DBTesting.java
@@ -1,5 +1,6 @@
 package com.database.testing;
 
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -18,7 +19,7 @@ public class DBTesting{
         Class.forName ("com.mysql.jdbc.Driver");
         System.out.println ("driver loaded");
 
-        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/emp_data",
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/spotify_data",
                 "root","root");
 
         System.out.println ("connected my database");
@@ -26,23 +27,43 @@ public class DBTesting{
     }
 
     @Test
-    public void Insert_data_In_To_employee_data_Table() throws SQLException {
+    public void Insert_data_In_To_employee_data_Table()  {
+        try {
+            sqlQuery = "insert into employees_data (id, firstName, mobile_number, salary) " +
+                    "values(1, 'Prahlad', '8052636931', 40000.00), (2, 'ravi', '9450807415', 30000.00)," +
+                    "(3, 'jay', '9995559900', 35000.00);";
 
-        sqlQuery = "insert into employee_data values ('rakesh','rkp@gmail.com')";
-        statement.executeUpdate (sqlQuery);
+            int noOfRowsAffected = statement.executeUpdate(sqlQuery);
+            Assert.assertEquals(noOfRowsAffected, 3);
 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void updateData_Into_employee_data_Table() throws SQLException {
-        sqlQuery = "update employee_data set emailId = 'rakesh@gmail.com ' where firstName = 'rakesh' ";
-        statement.executeUpdate (sqlQuery);
+        try {
+            sqlQuery = "update employees_data set mobile_number = '8888888888' where id = '2';";
+            int noOfRowsAffected = statement.executeUpdate(sqlQuery);
+
+            Assert.assertEquals(noOfRowsAffected, 1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void test_WhenAData_IsDeleted_FromATable_OfDatabase() throws SQLException {
-        sqlQuery = "delete from  employee_data where firstName = 'rakesh' ";
-        statement.executeUpdate (sqlQuery);
+        try {
+
+            sqlQuery = "delete from employees_data where id = '3'";
+            int noOfRowsAffected = statement.executeUpdate(sqlQuery);
+            Assert.assertEquals(noOfRowsAffected, 1);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @AfterTest
